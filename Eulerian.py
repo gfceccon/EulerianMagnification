fb14a8ea9d3afb0f421d55c1c2d8bbfc52f84abd
Garunts <gasbicalho@gmail.com>
2017-06-21 13:53:23 -0300

    Eulerian.py applies the transformation to any video

--- Eulerian.py
+++ Eulerian.py
@@ -10,27 +10,6 @@ from pyfftw.interfaces.scipy_fftpack import fft
 from pyfftw.interfaces.scipy_fftpack import ifft
 from scipy.fftpack import fftfreq
 
-def cvResizeAspectFill(src, maxSize,
-						upInterpolation=cv2.INTER_LANCZOS4,
-						downInterpolation=cv2.INTER_AREA):
-	h, w = src.shape[:2]
-	if w > h:
-		if w > maxSize:
-			interpolation=downInterpolation
-		else:
-			interpolation=upInterpolation
-		h = int(maxSize * h / float(w))
-		w = maxSize
-	else:
-		if h > maxSize:
-			interpolation=downInterpolation
-		else:
-			interpolation=upInterpolation
-		w = int(maxSize * w / float(h))
-		h = maxSize
-	dst = cv2.resize(src, (w, h), interpolation=interpolation)
-	return dst
-	
 def cvResizeCapture(capture, preferredSize):
 	# Try to set the requested dimensions.
 	w, h = preferredSize
@@ -46,29 +25,37 @@ def cvResizeCapture(capture, preferredSize):
 	h = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
 	return (w, h)
 
-def wxBitmapFromCvImage(image):
-	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-	h, w = image.shape[:2]
-	bitmap = wx.Bitmap.FromBuffer(w, h, image)
-	return bitmap
-
-class LazyEyes(wx.Frame):
-	def __init__(self, maxHistoryLength=200,
+#maxHistoryLength has to be larger or equal to 100
+#output has to be a .avi file
+class LazyEyes(object):
+	def __init__(self, 
+		source,
+		output,
+		maxHistoryLength=100,
 		minHz=5.0/6.0, maxHz=1.0,
 		amplification=32.0, numPyramidLevels=1,
 		useLaplacianPyramid=False,
 		useGrayOverlay=True,
 		numFFTThreads = 8, numIFFTThreads=8,
-		cameraDeviceID=0, imageSize=(640, 480),
-		title='Lazy Eyes'):
+		cameraDeviceID=0, imageSize=(640, 480)):
 	
 		self.mirrored = True
 		self._running = True
-		self._capture = cv2.VideoCapture(cameraDeviceID)
+		#self._capture = cv2.VideoCapture(0)
+		self._capture = cv2.VideoCapture(source)
+		self._fps = self._capture.get(cv2.CAP_PROP_FPS)
+		self._numFrames = int(self._capture.get(cv2.CAP_PROP_FRAME_COUNT))
+		self._currentFrame = 1
+		
+		#This only works if the source is a webcam
 		size = cvResizeCapture(self._capture, imageSize)
-		w, h = size
-		self._imageWidth = w
-		self._imageHeight = h
+		w, h = imageSize
+		
+		self._imageWidth = imageSize[0]
+		self._imageHeight = imageSize[1]
+		
+		fourcc = cv2.VideoWriter_fourcc(*'XVID')
+		self._out = cv2.VideoWriter(output, fourcc, 20.0, (w,h))
 		
 		self._useGrayOverlay = useGrayOverlay
 		if useGrayOverlay:
@@ -79,12 +66,10 @@ class LazyEyes(wx.Frame):
 			historyShape = (maxHistoryLength,
 			h >> numPyramidLevels,
 			w >> numPyramidLevels, 3)
-			
-		#print(historyShape)
 		
 		self._maxHistoryLength = maxHistoryLength
+		self._currentHistoryLength = 0
 		self._history = np.empty(historyShape, np.float32)
-		self._historyTimestamps = collections.deque()
 		
 		self._numPyramidLevels = numPyramidLevels
 		self._useLaplacianPyramid = useLaplacianPyramid
@@ -96,45 +81,24 @@ class LazyEyes(wx.Frame):
 		
 		pyfftw.interfaces.cache.enable()
 		pyfftw.interfaces.cache.set_keepalive_time(1.0)
-		
-		style = wx.CLOSE_BOX | wx.MINIMIZE_BOX | \
-				wx.CAPTION | wx.SYSTEM_MENU | \
-				wx.CLIP_CHILDREN
-				
-		wx.Frame.__init__(self, None, title=title, style=style, size=size)
-		self.Bind(wx.EVT_CLOSE, self._onCloseWindow)
-		quitCommandID = wx.NewId()
-		self.Bind(wx.EVT_MENU, self._onQuitCommand, id=quitCommandID)
-		acceleratorTable = wx.AcceleratorTable([(wx.ACCEL_NORMAL, wx.WXK_ESCAPE, quitCommandID)])
-		self.SetAcceleratorTable(acceleratorTable)
-		self._staticBitmap = wx.StaticBitmap(self, size=size)
-		
-		self._showImage(None)
-		rootSizer = wx.BoxSizer(wx.VERTICAL)
-		rootSizer.Add(self._staticBitmap)
-		self.SetSizerAndFit(rootSizer)
-		self._captureThread = threading.Thread(target=self._runCaptureLoop)
-		self._captureThread.start()
-		
-	def _onCloseWindow(self, event):
-		self._running = False
-		self._captureThread.join()
-		pyfftw.interfaces.cache.disable()
-		self.Destroy()
-		
-	def _onQuitCommand(self, event):
-		self.Close()
-		
-	def _runCaptureLoop(self):
-		while self._running:
+	
+	def _runMagnification(self):
+		success, image = self._capture.read()
+		while success:
+			image = cv2.resize(image, (self._imageWidth, self._imageHeight), image, interpolation=cv2.INTER_CUBIC);
+			#print(image.shape)
+			if self._applyEulerianVideoMagnification(image) == True:
+				self._out.write(image)
+				print("went")
+			else:
+				print("not")
+			del image
 			success, image = self._capture.read()
-			if image is not None:
-				self._applyEulerianVideoMagnification(image)
-				if (self.mirrored):
-					image[:] = np.fliplr(image)
-			wx.CallAfter(self._showImage, image)
-			
+	
 	def _applyEulerianVideoMagnification(self, image):
+		print("Frame", self._currentFrame, "of", self._numFrames)
+		self._currentFrame += 1
+		
 		timestamp = timeit.default_timer()
 		if self._useGrayOverlay:
 			smallImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY).astype(np.float32)
@@ -150,37 +114,21 @@ class LazyEyes(wx.Frame):
 		if self._useLaplacianPyramid:
 			smallImage[:] -= cv2.pyrUp(cv2.pyrDown(smallImage))
 		
-		
-		historyLength = len(self._historyTimestamps)
-		if historyLength < self._maxHistoryLength - 1:
-			print(historyLength, self._maxHistoryLength)
-			# Append the new image and timestamp to the
-			# history.
-			self._history[historyLength] = smallImage
-			self._historyTimestamps.append(timestamp)
-			# The history is still not full, so wait.
-			return
+		historyLength = len(self._history)
+		self._currentHistoryLength += 1
 		
 		if historyLength == self._maxHistoryLength - 1:
 			# Append the new image and timestamp to the
 			# history.
 			self._history[historyLength] = smallImage
-			self._historyTimestamps.append(timestamp)
 		else:
 			# Drop the oldest image and timestamp from the
 			# history and append the new ones.
 			self._history[:-1] = self._history[1:]
-			self._historyTimestamps.popleft()
 			self._history[-1] = smallImage
-			self._historyTimestamps.append(timestamp)
 		
 		# The history is full, so process it.
-		# Find the average length of time per frame.
-		startTime = self._historyTimestamps[0]
-		endTime = self._historyTimestamps[-1]
-		timeElapsed = endTime - startTime
-		timePerFrame = timeElapsed / self._maxHistoryLength
-		#print 'FPS:', 1.0 / timePerFrame
+		timePerFrame = 1 / self._fps
 		
 		fftResult = fft(self._history, axis=0, threads=self._numFFTThreads)
 		
@@ -193,8 +141,7 @@ class LazyEyes(wx.Frame):
 		
 		ifftResult = ifft(fftResult, axis=0, threads=self._numIFFTThreads)
 		
-		# Amplify the result and overlay it on the
-		# original image.
+		# Amplify the result and overlay it on the original image.
 		overlay = np.real(ifftResult[-1]) * self._amplification
 		i = 0
 		while i < self._numPyramidLevels:
@@ -203,23 +150,13 @@ class LazyEyes(wx.Frame):
 		if self._useGrayOverlay:
 			overlay = cv2.cvtColor(overlay, cv2.COLOR_GRAY2BGR)
 		cv2.convertScaleAbs(image + overlay, image)
-			
-	def _showImage(self, image):
-		if image is None:
-			# Provide a black bitmap.
-			bitmap = wx.Bitmap(self._imageWidth, self._imageHeight)
-		else:
-			# Convert the image to bitmap format.
-			bitmap = wxBitmapFromCvImage(image)
-		# Show the bitmap.
-		self._staticBitmap.SetBitmap(bitmap)
 		
-		
+		return self._currentHistoryLength > self._maxHistoryLength	
+
 def main():
-	app = wx.App()
-	lazyEyes = LazyEyes()
-	lazyEyes.Show()
-	app.MainLoop()
+	lazyEyes = LazyEyes("cam.avi", "output.avi")
+	lazyEyes._runMagnification()
+	
 
 if __name__ == '__main__':
 	main()
